77295d1e4f3e454f865b953aec7749c72a6b4428
mjerta <maarten.postma1000@gmail.com>
2024-12-21 15:30:46 +0100

    Creating class Line with some fields

---
+++ day7/src/main/java/nl/mpdev/Line.java
@@ -0,0 +1,15 @@
+package nl.mpdev;
+
+import java.util.List;
+
+public class Line {
+  private Integer output;
+  private List<Integer> numbersToCalculate;
+
+  public Line(String input) {
+    this.output = output;
+    this.numbersToCalculate = numbersToCalculate;
+  }
+
+
+}
