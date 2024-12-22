38375c4df40dd53d43bd7208050eb7117abd76e6
mjerta <maarten.postma1000@gmail.com>
2024-12-22 09:55:29 +0100

    Create class FileReader that return all lines of puzzle into a list of lines

--- day7/src/main/java/nl/mpdev/Main.java
+++ day7/src/main/java/nl/mpdev/Main.java
@@ -1,7 +1,18 @@
 package nl.mpdev;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    FileReader fileReader = new FileReader("./puzzle.txt");
+    List<String> linesOfFile = fileReader.getLines();
+
+    for(String line: linesOfFile) {
+      System.out.println(line);
+    }
+
   }
 }
