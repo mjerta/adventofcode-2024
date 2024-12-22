8c7c9cccebc7dcc92939e43390c341b293f07449
mjerta <maarten.postma1000@gmail.com>
2024-12-22 12:38:10 +0100

    Adding a recursive method to create every possibility of using the operators between the numbers

--- day7/src/main/java/nl/mpdev/Line.java
+++ day7/src/main/java/nl/mpdev/Line.java
@@ -3,10 +3,11 @@ package nl.mpdev;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class Line {
   private final Integer output;
-  private List<Integer> numbersToCalculate;
+  private final List<Integer> numbersToCalculate;
 
   public Line(String input) {
     numbersToCalculate = new ArrayList<>();
@@ -23,5 +24,25 @@ public class Line {
 
   public List<Integer> getNumbersToCalculate() {
     return numbersToCalculate;
+  }
+
+  public void calculateBasedOnOperator(
+    int index,
+    int currentResult,
+    Set<Integer> results,
+    boolean isAddition) {
+    if(index == numbersToCalculate.size()){
+      results.add(currentResult);
+      return;
+    }
+    int currentNumber = numbersToCalculate.get(index);
+    if (isAddition) {
+      calculateBasedOnOperator(index + 1, currentResult + currentNumber,  results, true);
+      calculateBasedOnOperator(index + 1, currentResult * currentNumber,  results, false);
+    } else {
+      calculateBasedOnOperator(index + 1, currentResult * currentNumber,  results, false);
+      calculateBasedOnOperator(index + 1, currentResult + currentNumber,  results, true);
+    }
+
   }
 }
--- day7/src/main/java/nl/mpdev/Main.java
+++ day7/src/main/java/nl/mpdev/Main.java
@@ -1,17 +1,28 @@
 package nl.mpdev;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
     FileReader fileReader = new FileReader("./puzzle.txt");
     List<String> linesOfFile = fileReader.getLines();
 
+
     for(String singleLine: linesOfFile) {
       Line line = new Line(singleLine);
       System.out.println(line.getOutput());
       System.out.println(line.getNumbersToCalculate());
-    }
+      Set<Integer> results = new HashSet<>();
+      line.calculateBasedOnOperator(0, 0, results , true);
+      line.calculateBasedOnOperator(0, 1, results , false);
+      System.out.println(results);
+      for(int number : line.getNumbersToCalculate()) {
+        results.contains(number);
 
+      }
+    }
   }
 }
