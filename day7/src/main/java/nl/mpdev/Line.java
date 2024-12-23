5307d6aa1b4ef0045266ec2f6ad8ccf5d18a2b57
mjerta <maarten.postma1000@gmail.com>
2024-12-23 17:40:15 +0100

    In the middle to get the logic in for the second part:

--- day7/src/main/java/nl/mpdev/Line.java
+++ day7/src/main/java/nl/mpdev/Line.java
@@ -7,14 +7,25 @@ import java.util.List;
 public class Line {
   private final Long output;
   private final List<Long> numbersToCalculate;
+  private final List<Long> numbersToCalculateFirstPart;
+  private final List<List<Long>> numbersToCalculateSecondPart;
   private final String[] operators = {"+", "*"};
   private final List<Long> results;
+  private final List<Long> resultsNumbersToCalculateFirstPart;
+  private final List<Long> resultsNumbersToCalculateSecondPart;
+  private List<Long> firstArrayItemNumberToCalculateSecondPart;
   private boolean hasValidNumber = false;
+  private String firstPartWithFirstItemSecondPart;
 
   //constructor
   public Line(String input) {
     numbersToCalculate = new ArrayList<>();
+    numbersToCalculateFirstPart = new ArrayList<>();
+    firstArrayItemNumberToCalculateSecondPart = new ArrayList<>();
+    numbersToCalculateSecondPart = new ArrayList<>();
     results = new ArrayList<>();
+    resultsNumbersToCalculateFirstPart = new ArrayList<>();
+    resultsNumbersToCalculateSecondPart = new ArrayList<>();
     var splittedString = input.split(":");
     this.output = Long.parseLong(splittedString[0]);
     var splittedNumbersAfterOutput = splittedString[1].trim().split("\\s+");
@@ -23,7 +34,6 @@ public class Line {
   }
 
   public void executeCalculations(long currentIndex, long currentValue) {
-    // Base case: If we've processed all the numbers, print the current value (or store it)
     if (currentIndex == numbersToCalculate.size()) {
       results.add(currentValue);
       return;
@@ -36,13 +46,84 @@ public class Line {
         executeCalculations(currentIndex + 1, currentValue * numbersToCalculate.get((int) currentIndex));
       }
     }
-    validateResults();
   }
 
-  private void validateResults() {
+  public void executeCalculations(long currentIndex, long currentValue, List<Long> numbersToCalculate) {
+    if (numbersToCalculate.size() == 1) {
+      return;
+    }
+    if (currentIndex == numbersToCalculate.size()) {
+      resultsNumbersToCalculateFirstPart.add(currentValue);
+      return;
+    }
+    for (String operator : operators) {
+      if (operator.equals("+")) {
+        executeCalculations(currentIndex + 1, currentValue + numbersToCalculate.get((int) currentIndex), numbersToCalculate);
+      }
+      else if (operator.equals("*")) {
+        executeCalculations(currentIndex + 1, currentValue * numbersToCalculate.get((int) currentIndex), numbersToCalculate);
+      }
+    }
+  }
+
+  public void addConcatenation() {
+    for (int i = 1; i <= numbersToCalculate.size(); i++) {
+      if (i % 2 != 0) {
+        numbersToCalculateFirstPart.clear();
+        var firstPart = numbersToCalculate.subList(0, i);
+        // Not needed but just to display
+        var firstItemFromSecondPart = numbersToCalculate.get(i);
+        firstArrayItemNumberToCalculateSecondPart.add(firstItemFromSecondPart);
+        var secondPart = numbersToCalculate.subList(i + 1, numbersToCalculate.size());
+        System.out.print(firstPart);
+        System.out.print(" || ");
+        System.out.print(firstItemFromSecondPart + " ");
+        if (!secondPart.isEmpty()) {
+          System.out.print(secondPart);
+        }
+        numbersToCalculateFirstPart.addAll(firstPart);
+        if (!secondPart.isEmpty()) {
+          numbersToCalculateSecondPart.add(secondPart);
+        }
+        executeCalculations(1, numbersToCalculateFirstPart.getFirst(), numbersToCalculateFirstPart);
+        for (Long result : resultsNumbersToCalculateFirstPart) {
+          System.out.println();
+          var test = result.toString() + firstItemFromSecondPart;
+          System.out.println(test);
+          if (!secondPart.isEmpty()) {
+            List<Long> anotherList = new ArrayList<>();
+            anotherList.add(Long.parseLong(test));
+            anotherList.addAll(secondPart);
+            System.out.println(anotherList);
+          }
+        }
+      }
+      if (i % 2 == 0) {
+        // Not needed but just to display
+        var firstPart = numbersToCalculate.subList(0, i);
+        var firstItemFromSecondPart = numbersToCalculate.get(i);
+        firstArrayItemNumberToCalculateSecondPart.add(firstItemFromSecondPart);
+        var secondPart = numbersToCalculate.subList(i + 1, numbersToCalculate.size());
+        if (!secondPart.isEmpty()) {
+          numbersToCalculateSecondPart.add(secondPart);
+        }
+        System.out.print(firstPart);
+        System.out.print(" || ");
+        System.out.print(firstItemFromSecondPart + " ");
+        if (!secondPart.isEmpty()) {
+          System.out.print(secondPart);
+        }
+
+      }
+      System.out.println();
+    }
+  }
+
+  public void validateResults() {
+//    System.out.println(results);
     if (!results.isEmpty()) {
       for (long result : results) {
-        if(output.equals(result)) {
+        if (output.equals(result)) {
           hasValidNumber = true;
           break;
         }
@@ -60,5 +141,17 @@ public class Line {
 
   public boolean isHasValidNumber() {
     return hasValidNumber;
+  }
+
+  public List<Long> getResultsNumbersToCalculateFirstPart() {
+    return resultsNumbersToCalculateFirstPart;
+  }
+
+  public List<Long> getFirstArrayItemNumberToCalculateSecondPart() {
+    return firstArrayItemNumberToCalculateSecondPart;
+  }
+
+  public List<List<Long>> getNumbersToCalculateSecondPart() {
+    return numbersToCalculateSecondPart;
   }
 }
--- day7/src/main/java/nl/mpdev/Main.java
+++ day7/src/main/java/nl/mpdev/Main.java
@@ -11,12 +11,22 @@ public class Main {
     // Loop through each line in the file
     for (String singleLine : linesOfFile) {
       Line line = new Line(singleLine);
+      System.out.println(line.getOutput());
+      System.out.println(line.getNumbersToCalculate());
       line.executeCalculations(1, line.getNumbersToCalculate().getFirst());
+
+      line.validateResults();
 
       if (line.isHasValidNumber()) {
         numbersThatWhereTrue += line.getOutput();
       }
+      line.addConcatenation();
+      System.out.println("result from uneven part" + line.getResultsNumbersToCalculateFirstPart());
+      System.out.println(line.getFirstArrayItemNumberToCalculateSecondPart());
+      System.out.println(line.getNumbersToCalculateSecondPart());
+
     }
-    System.out.println(numbersThatWhereTrue);
+
+//    System.out.println(numbersThatWhereTrue);
   }
 }
